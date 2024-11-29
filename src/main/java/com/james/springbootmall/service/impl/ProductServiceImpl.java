737b55d8e5a5299764512fbd414feaf3471c2eb8
james <james918310@gmail.com>
2024-11-29 14:26:13 +0800

    商品功能 實作刪除商品

--- src/main/java/com/james/springbootmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/james/springbootmall/service/impl/ProductServiceImpl.java
@@ -31,5 +31,10 @@ public class ProductServiceImpl implements ProductService {
         productDao.updateProduct(productId,productRequest);
     }
 
+    @Override
+    public void deleteProductById(Integer productId) {
+        productDao.deleteProductById(productId);
+    }
+
 
 }
